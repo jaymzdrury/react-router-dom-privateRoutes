dcd48534c747d719ce0aae48c45399acae8dc8d5
James Drury <jdrury@Jamess-MacBook-Air.local>
2022-11-23 12:33:33 -0500

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+reportWebVitals();
