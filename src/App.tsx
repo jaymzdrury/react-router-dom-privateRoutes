43999e1b5747e159be9913a6bdf4d89b4bcc20ff
James Drury <jdrury@Jamess-MacBook-Air.local>
2022-11-23 13:54:41 -0500

    added suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import {useState} from 'react'
+import {useState, Suspense, lazy} from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Login from './Login'
-import Main from './Main'
 import PrivateRoute from './PrivateRoute'
+const Login = lazy(() => import('./Login'))
+const Main = lazy(() => import('./Main'))
 function App() {
-
   const [loggedIn, loggedInSet] = useState(false)
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={<PrivateRoute loggedIn={loggedIn} />}>
-          <Route path='/' element={<Main />} />
+          <Route path='/' element={<Suspense fallback={<div>Loading...</div>}><Main /></Suspense>} />
         </Route>
-        <Route path='/login' element={<Login loggedInSet={loggedInSet} />} />
+        <Route path='/login' element={<Suspense fallback={<div>Loading...</div>}><Login loggedInSet={loggedInSet} /></Suspense>} />
       </Routes>
     </Router>
   );
